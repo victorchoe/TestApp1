4ae71a7cf3a96451fc7e96005e7a24617f34ebdc
cheolwon, choi <ccw44@naver.com>
2024-12-30 20:51:35 +0900

    home1

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -4,4 +4,5 @@ class Program {
     public static void Main(String[] args) {
         Console.WriteLine("Hello world");
         Console.WriteLine("aaaa");
+        Console.WriteLine("add: home test");
 }
