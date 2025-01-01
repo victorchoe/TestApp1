5dd502d7e6ad78ed44f0e0cfa4263b7d639b1905
cheolwon, choi <ccw44@naver.com>
2025-01-01 19:46:34 +0900

    20250101

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 
-class Program {
-    public static void Main(String[] args) {
+class Program
+{
+    public static void Main(String[] args)
+    {
         Console.WriteLine("Hello world");
         Console.WriteLine("aaaa");
         Console.WriteLine("add: home test");
+        Console.WriteLine("happy new year at home");
+    }
 }
