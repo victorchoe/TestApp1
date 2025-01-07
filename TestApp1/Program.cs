62f71b147e58d6190f680d08f3f8b27c59076c80
cheolwon, choi <ccw44@naver.com>
2025-01-07 20:59:24 +0900

    Revert "2025010701"
    This reverts commit b51c7276840430e815c996c078bdf3f6b4814053.

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -10,8 +10,5 @@ class Program
         Console.WriteLine("happy new year at home");
 <<<<<<< HEAD
         //근데 이상하다. 깃 허브에 솔루션(sln) 파일이 또 생겼다.
-=======
-        Console.WriteLine("20250107add");
->>>>>>> b51c7276840430e815c996c078bdf3f6b4814053
     }
 }
