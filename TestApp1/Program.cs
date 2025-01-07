8125d26d67d704684593a41ff9f0648535abdc4c
cheolwon, choi <ccw44@naver.com>
2025-01-07 21:00:30 +0900

    addfff11

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -8,7 +8,6 @@ class Program
         Console.WriteLine("aaaa");
         Console.WriteLine("add: home test");
         Console.WriteLine("happy new year at home");
-<<<<<<< HEAD
-        //근데 이상하다. 깃 허브에 솔루션(sln) 파일이 또 생겼다.
+        Console.WriteLine("home commit!!");
     }
 }
